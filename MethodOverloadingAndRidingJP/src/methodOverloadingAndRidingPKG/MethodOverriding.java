2ac02f4366c37f965f7bbec489e23b22d75a6ffe
SujitMK <32028914+SujitMK@users.noreply.github.com>
2017-10-04 21:04:21 +0530

    Update MethodOverriding.java

--- MethodOverloadingAndRidingJP/src/methodOverloadingAndRidingPKG/MethodOverriding.java
+++ MethodOverloadingAndRidingJP/src/methodOverloadingAndRidingPKG/MethodOverriding.java
@@ -1,26 +1,26 @@
-package methodOverloadingAndRidingPKG;
+package methodOverloadingAndRidingPKG;          // declared a package
 
-public class MethodOverriding extends MethodOverloading{
+public class MethodOverriding extends MethodOverloading{          // Declared a class which extends another class
 	
 	
 	// Addition Method with two parameters overridden 
 	public int addtion(int a, int b)
 	{
-		int bonus = 1000;
-		int c = a+b+bonus;
-		System.out.println();
-		System.out.println("Addition of Int A,B and Bonus(1000) is : "+ c);
-		return c;
+		int bonus = 1000;         // declared and initialized a variable
+		int c = a+b+bonus;          // declared a variable and calculating a sum
+		System.out.println();         // printing blank line
+		System.out.println("Addition of Int A,B and Bonus(1000) is : "+ c); // printing info
+		return c;            // returing a int type value
 	}
 	
 	// Addition Method with three parameters overridden 
 	public int addtion(int a, int b, int c)
 	{
-		int bonus = 1000;
-		int d = a+b+c+bonus;
-		System.out.println();
-		System.out.println("Addition of Int A, B, C and Bonus(1000) is : "+ d);
-		return d;
+		int bonus = 1000;             // declared and initialized a variable
+		int d = a+b+c+bonus;                // declared a variable and calculating a sum
+		System.out.println();             // printing blank line
+		System.out.println("Addition of Int A, B, C and Bonus(1000) is : "+ d); // printing info
+		return d;                 // returing a int type value
 	}
 
 }
