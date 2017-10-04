9585fdc5a0b01db9424c5175097952c84412a7d9
SujitMK <32028914+SujitMK@users.noreply.github.com>
2017-10-04 21:05:21 +0530

    Update MethodOverloading.java

--- MethodOverloadingAndRidingJP/src/methodOverloadingAndRidingPKG/MethodOverloading.java
+++ MethodOverloadingAndRidingJP/src/methodOverloadingAndRidingPKG/MethodOverloading.java
@@ -1,33 +1,33 @@
-package methodOverloadingAndRidingPKG;
+package methodOverloadingAndRidingPKG;            // Package declared 
 
-public class MethodOverloading {
+public class MethodOverloading {                  // Method overloading class declared and defined 
 	
 	
 	// Addition Method with two int parameters
 	public int addition(int a, int b)
 	{
-		int c = a+b;
-		System.out.println();
-		System.out.println("Addition of Int A and B is : "+ c);
-		return c;
+		int c = a+b;        // Adding a and b
+		System.out.println();  // printing blank line
+		System.out.println("Addition of Int A and B is : "+ c);    // printing info
+		return c;           //returning an int type value
 	}
 	
 	// Addition Method with three int parameters
 	public int addition(int a, int b, int c)
 	{
-		int d = a+b+c;
-		System.out.println();
-		System.out.println("Addition of Int A, B and C is : "+ d);
-		return d;
+		int d = a+b+c;                 // calculating sum
+		System.out.println();   // printing blank line
+		System.out.println("Addition of Int A, B and C is : "+ d); // printing info
+		return d;                  //returning an int type value
 	}
 	
 	// Addition Method with two int and one double parameters
 	public double addition(int a, int b, double d)
 	{
-		d = a+b+d;
-		System.out.println();
-		System.out.println("Addition of Int A, B and Double D is : "+ d);
-		return d;
+		d = a+b+d;                 // calculating sum
+		System.out.println();   // printing blank line
+		System.out.println("Addition of Int A, B and Double D is : "+ d); // printing info
+		return d;            //returning an int type value
 	}
 
 }
